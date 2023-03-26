6d73bd90f3cf5d66c01a9eb25518e1ea5649aa67
Rodrigo <rodrigo.ribeiro@q2pay.com.br>
2023-03-25 23:59:49 -0300

    feat: add navigator and fix eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import * as React from 'react';
+import {Home} from './pages/home';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-export const App = (): JSX.Element => {
-  return (
-    <View>
-      <Text>Hello world!</Text>
-    </View>
-  );
-};
+const Stack = createNativeStackNavigator();
+export const App = (): JSX.Element => (
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Group screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Group>
+    </Stack.Navigator>
+  </NavigationContainer>
+);
