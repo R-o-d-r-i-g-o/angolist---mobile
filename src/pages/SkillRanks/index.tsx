da7e034518c5a08461ffcb34d848dec4b666acef
Rodrigo <rodrigo.ribeiro@q2pay.com.br>
2023-04-25 21:53:39 -0300

    feat: add grath

--- src/pages/SkillRanks/index.tsx
+++ src/pages/SkillRanks/index.tsx
@@ -1,13 +1,64 @@
 import React from 'react';
+import color from '../../utils/colors';
 import {PageTemplate} from '../../templates/pageTemplate';
-import {View, Text} from 'react-native';
+import {AreaChart, Grid} from 'react-native-svg-charts';
+
+import {Circle} from 'react-native-svg';
+import {Path} from 'react-native-svg';
+
+interface DecoratorProps {
+  x: (arg: number) => number;
+  y: (arg: number) => number;
+  data: number[];
+}
+
+interface LineProps {
+  line: string;
+}
+
+export const Line = (props: Partial<LineProps>) => {
+  const {line} = props as LineProps;
+  return <Path d={line} stroke={color.baseWhite} fill={'none'} />;
+};
+
+export const Dots = (props: Partial<DecoratorProps>) => {
+  const {x, y, data} = props as DecoratorProps;
+  return (
+    <>
+      {data?.map((value, index) => {
+        return (
+          value && (
+            <Circle
+              key={index}
+              cx={x(index)}
+              cy={y(value)}
+              r={5}
+              stroke={color.baseWhite}
+              fill={color.baseBrown}
+            />
+          )
+        );
+      })}
+    </>
+  );
+};
 
 export const SkillRanks = () => {
+  const data = [null, 30, 10, 30, 20, 30, null]; // salvar com null nas extremidades pra não quebrar o front
+
   return (
     <PageTemplate mainText={'Pontuação'}>
-      <View>
-        <Text>fjsklfjkskllk</Text>
-      </View>
+      <AreaChart
+        style={{height: 200, width: '94%'}}
+        data={data}
+        gridMin={-5}
+        contentInset={{top: 30, bottom: 30}}
+        // svg={{fill: '#ADD8E6'}}
+        gridMax={40}>
+        <Grid />
+        <Line />
+        <Dots />
+      </AreaChart>
     </PageTemplate>
   );
 };
